a223cb07d986acaf04609207fad59ecb9b0f9afe
aryan-marthak <aryan.marthak1@gmail.com>
2024-03-09 00:38:34 +0530

    functions

---
+++ vid_58.js
@@ -0,0 +1,30 @@
+function nice(name) {
+  console.log("hey " + name + " you are nice");
+  console.log("hey " + name + " you are nice");
+  console.log("hey " + name + " you are nice");
+  console.log("hey " + name + " you are nice");
+  console.log("hey " + name + " you are nice");
+}
+
+// nice("aryan");
+// nice("mama");
+
+function sum(a,b){
+    // console.log(a+b)
+    return a + b
+}
+
+// sum(5, 5)
+result = sum(5,9)
+console.log("the sum of these number is: ", result)
+
+
+const func1 = (x)=>{
+  console.log("i am a arrow function " + x)
+}
+
+func1(42);
+func1(4277);k
+func1(52);
+func1(422);
+
---
+++ vid_60_strings.js
@@ -0,0 +1,18 @@
+console.log("this is strings tutorial")
+
+let a = "aryan"
+console.log(a)
+// console.log(a[3])
+
+console.log(a.length)
+
+let name = "aryan"
+let food = "chips"
+
+console.log("i am " + name + " and i like to eat " + food);
+console.log(`i am ${name} and i love eating ${food}`);
+
+let b = "chocolate"
+
+console.log(b.toLowerCase())
+console.log(b.toUpperCase())
