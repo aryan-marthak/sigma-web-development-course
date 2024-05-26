e054eb1b423e8cc8c039526be126de7ba5881ea4
aryan-marthak <aryan.marthak1@gmail.com>
2024-05-26 23:56:09 +0530

    basics of express

---
+++ vid_89/main.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+  console.log("hey its a get request");
+  res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  console.log("hey its a post request");
+  res.send("Hello World post!");
+});
+
+app.put("/", (req, res) => {
+  console.log("hey its a put request");
+  res.send("Hello World put!");
+});
+
+app.get("/index", (req, res) => {
+  console.log("hey its index");
+  res.sendFile('templates/index.html', {root: __dirname});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
